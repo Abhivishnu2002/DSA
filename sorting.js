3f55638ccde37f601d929a7689edb4ae33e214e4
Abhivishnu2002 <abhijithvishnu180@gmail.com>
2025-07-21 12:21:24 +0530

    sorting 3

--- sorting.js
+++ sorting.js
@@ -30,3 +30,25 @@ function insertionSort(arr){
 }
 insertionSort(arr);
 console.log(arr)
+
+function mergeSort(arr){
+    if(arr.length < 2) return arr;
+    let mid = Math.floor(arr.length/2);
+    let leftArr = arr.slice(0, mid);
+    let rightArr = arr.slice(mid);
+    return merge(mergeSort(leftArr), mergeSort(rightArr));
+}
+
+function merge(leftArr, rightArr){
+    let sortedArr = [];
+    while(leftArr.length && rightArr.length){
+        if(leftArr[0] <= rightArr[0]){
+            sortedArr.push(leftArr.shift());
+        }else{
+            sortedArr.push(rightArr.shift());
+        }
+    }
+    return [...sortedArr, ...leftArr, ...rightArr];
+}
+
+console.log(mergeSort([-6,20,4,-2,8]));
